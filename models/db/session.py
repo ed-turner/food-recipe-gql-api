a5711cfce64fa7a62d6218e78e257c21afd2b6fe
Edward Turner <edward.turnerr@gmail.com>
2022-07-07 10:15:07 -0600

    trying to develop async SQL session GraphQL

--- models/db/session.py
+++ models/db/session.py
@@ -1,5 +1,7 @@
 
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio.engine import create_async_engine
 from sqlalchemy import create_engine
 
 
@@ -7,6 +9,9 @@ from settings import Settings
 
 engine = create_engine(Settings().DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
+
+async_engine = create_async_engine(Settings().DATABASE_URL)
+AsyncSessionLocal = sessionmaker(bind=async_engine, class_=AsyncSession)
 
 
 def get_session():
@@ -27,3 +32,19 @@ def get_session():
 
     finally:
         session.close()
+
+
+async def get_async_session():
+    """
+
+    :return:
+    """
+
+    async with AsyncSessionLocal() as session:
+        async with session.begin():
+            try:
+                yield session
+            except Exception as e:
+                await session.rollback()
+
+                raise e
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,56 @@
-import json
+from graphene.relay.node import to_global_id
+
+
+def test_get_recipe1(api_client):
+
+    global_id = to_global_id("RecipeObjectType", 1)
+    _q = """
+        {
+            node(id: "%s") {
+                ... on RecipeObjectType {
+                    id
+                }
+            }
+        }
+        """ % global_id
+
+    response = api_client.post("/", json={"query": _q})
+
+    assert response.status_code == 200
+
+    assert response.json().get("errors", None) is None, response.json()["errors"][0]
+
+    try:
+        assert response.json()["data"]["node"]["id"] == global_id, response.content
+    except TypeError:
+        raise AssertionError(response.json()["errors"])
+
+
+def test_get_recipes(api_client):
+
+    _q = """
+        {
+            recipes
+                {
+                    edges {
+                        node {
+                            id
+                        }
+                    }
+                }
+        }
+        """
+
+    response = api_client.post("/", json={"query": _q})
+
+    assert response.status_code == 200
+
+    assert response.json().get("errors", None) is None, response.json()["errors"][0]
+
+    try:
+        assert len(response.json()["data"]["recipes"]["edges"]) == 2, response.content
+    except TypeError:
+        raise AssertionError(response.json()["errors"])
 
 
 def test_get_tags(api_client):
@@ -22,29 +74,3 @@ def test_get_tags(api_client):
     assert response.json().get("errors", None) is None, response.json()["errors"]
 
     assert len(response.json()["data"]["tags"]["edges"]) == 1, response.content
-
-
-def test_get_recipes(api_client):
-    _q = """
-        {
-                recipes
-                {
-                    edges {
-                        node {
-                            id
-                        }
-                    }
-                }
-            }
-        """
-
-    response = api_client.post("/", json={"query": _q})
-
-    assert response.status_code == 200
-
-    assert response.json().get("errors", None) is None, response.json()["errors"]
-
-    try:
-        assert len(response.json()["data"]["recipes"]["edges"]) == 2, response.content
-    except TypeError:
-        raise AssertionError(response.json()["errors"])
