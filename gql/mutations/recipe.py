aca636f482f1695a5b0d962f107edb957466f74a
Edward Turner <edward.turnerr@gmail.com>
2022-07-08 09:55:22 -0600

    downgrade graphene so I can use the filter

--- gql/mutations/recipe.py
+++ gql/mutations/recipe.py
@@ -5,59 +5,59 @@ from sqlalchemy.sql.expression import delete
 from models.db.recipe import Recipe
 from models.db.tags import Tags
 from models.db.associations import recipe_tags_table
-from gql.fields.inputs import RecipeInputType
+#from gql.fields.inputs import RecipeInputType
 
 
-class CreateRecipe(graphene.Mutation):
-    class Arguments:
-        recipe = RecipeInputType(required=True)
-
-    Output = graphene.types.String
-
-    @staticmethod
-    def mutate(parent, info, recipe):
-        """
-
-        :param parent:
-        :param info:
-        :param recipe:
-        :return:
-        """
-        db_session = info.context["session"]
-
-        db_recipe = Recipe()
-
-        db_recipe.name = recipe.name
-        db_recipe.description = recipe.description
-        db_recipe.direction = recipe.direction
-
-        # if len(recipe.recipeTags) == 0:
-        #     pass
-        # else:
-        #     tags = []
-        #
-        #     for tag in recipe.recipeTags:
-        #         _tag = db_session.query(Tags).filter(Tags.name == tag.name).first()
-        #
-        #         if _tag is None:
-        #             _tag = Tags(name=tag.name)
-        #             db_session.add(_tag)
-        #
-        #         tags.append(
-        #             _tag
-        #         )
-        #
-        #     db_recipe.recipe_tags = tags
-
-        db_session.add(db_recipe)
-
-        db_session.flush()
-
-        recipe_id = db_recipe.id
-
-        db_session.commit()
-
-        return to_global_id("Recipe", recipe_id)
+# class CreateRecipe(graphene.Mutation):
+#     class Arguments:
+#         recipe = RecipeInputType(required=True)
+#
+#     Output = graphene.types.String
+#
+#     @staticmethod
+#     def mutate(parent, info, recipe):
+#         """
+#
+#         :param parent:
+#         :param info:
+#         :param recipe:
+#         :return:
+#         """
+#         db_session = info.context["session"]
+#
+#         db_recipe = Recipe()
+#
+#         db_recipe.name = recipe.name
+#         db_recipe.description = recipe.description
+#         db_recipe.direction = recipe.direction
+#
+#         # if len(recipe.recipeTags) == 0:
+#         #     pass
+#         # else:
+#         #     tags = []
+#         #
+#         #     for tag in recipe.recipeTags:
+#         #         _tag = db_session.query(Tags).filter(Tags.name == tag.name).first()
+#         #
+#         #         if _tag is None:
+#         #             _tag = Tags(name=tag.name)
+#         #             db_session.add(_tag)
+#         #
+#         #         tags.append(
+#         #             _tag
+#         #         )
+#         #
+#         #     db_recipe.recipe_tags = tags
+#
+#         db_session.add(db_recipe)
+#
+#         db_session.flush()
+#
+#         recipe_id = db_recipe.id
+#
+#         db_session.commit()
+#
+#         return to_global_id("Recipe", recipe_id)
 
 
 class ModifyRecipeName(graphene.Mutation):
